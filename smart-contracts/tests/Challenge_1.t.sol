92f4b4dedc0c4d2d2b5cc0db0d57a4260b8c3d41
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-03 17:26:29 +0100

    feat: :sparkles: add challenge 1
    add challenge 1

--- smart-contracts/tests/Challenge_1.t.sol
+++ smart-contracts/tests/Challenge_1.t.sol
@@ -5,10 +5,19 @@ import 'forge-std/Test.sol';
 
 import '@smart-contracts/Challenge_1.sol';
 
-contract TestContract is Test {
+contract Challenge1 is Test {
   Contract c;
 
   function setUp() public {
     c = new Contract();
+  }
+
+  /// @notice Tests that the message returned from the contract is equal to "Hello World!"
+  /// @dev Functions that begin with the word 'test' are, well... tests!
+  function testHelloWorld() public {
+    /// @dev store the message of the smart contract in a variable in memory
+    string memory message = c.showMessage();
+    /// @dev assertEq(a, b) returns true if a === b
+    assertEq(message, 'Hello World!');
   }
 }
