247340c646eb4381d28456e79e20a453fd7d647d
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-03 17:57:30 +0100

    feat: add challenge 2
    add challenge 2

---
+++ smart-contracts/src/Challenge_2.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+/// @title Challenge 2
+/// @author Byont Labs
+/*
+ * The message in this contract has not been initialized yet. If you look at the test, it calls a function setMessage()
+ * that sets the message in this contract to 'Hello World!'. It is your job to add it to this contract!
+ * Hint:
+ * - The function should accept a string parameter in memory
+ * - The function should be public => https://docs.soliditylang.org/en/v0.8.17/contracts.html#function-visibility
+ * - The function should be payable => https://docs.alchemy.com/docs/solidity-payable-functions#
+ */
+
+contract Contract {
+  string message;
+
+  /// @return message:string; the message stored in the contract.
+  function getMessage() public view returns (string memory) {
+    return message;
+  }
+
+  /// @dev TODO: add setMessage() here!
+}
