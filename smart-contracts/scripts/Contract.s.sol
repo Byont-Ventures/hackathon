90c129e3aa84f212f8d4802618d3dddbf049e712
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-07 11:21:18 +0100

    feat: add challenge 4, add comment: auto detect solc to foundry.toml
    add challenge4, add comment: auto detect solc to foundry.toml

--- smart-contracts/scripts/Contract.s.sol
+++ smart-contracts/scripts/Contract.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity >=0.6.0 <0.9.0;
 
 import {Script} from 'forge-std/Script.sol';
 import {Contract} from '@smart-contracts/Contract.sol';
