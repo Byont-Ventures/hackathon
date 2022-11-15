fd4451635609f60dab0d6cf54b97715b9b369316
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-15 17:24:05 +0100

    feat: add challenge 12

---
+++ smart-contracts/src/Challenge_12.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.6.0 <0.9.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+/// @title Challenge 12
+/// @author Byont Labs
+/// @notice A barebones NFT contract example with documentation for learning purposes
+contract Challenge12 is ERC721, Ownable {
+  /// @dev Library that can convert uint256 to string. (by the way, uint is short for uint256)
+  using Strings for uint256;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    uint256 _maxSupply
+  ) ERC721(_name, _symbol) {
+    maxSupply = _maxSupply;
+  }
+
+  /// @notice The base uri for NFT metadata, should become something like ipfs://somestring/
+  /// @dev Private strings are NOT hidden for humans, just for other smart contracts
+  string private _baseURIextended;
+
+  /// @notice The total amount of minted NFT's
+  uint256 public totalSupply;
+
+  /// @notice The max amount of allowed NFT mints
+  uint256 public maxSupply;
+
+  /// @notice The max amount of allowed mints per user
+  uint256 public maxPerUser = 5;
+
+  /// @notice Keeps track of user mints
+  mapping (address => uint) public userMintCount;
+
+  /// @notice Sets the max mint per user
+  /// @param _amount The max allowed amount
+  function setUserMaxMint(uint _amount) public onlyOwner {
+    maxPerUser = _amount;
+  }
+
+  /// @notice Sets the base URI
+  /// @param _uri The base uri
+  function setBaseURI(string memory _uri) external onlyOwner {
+    _baseURIextended = _uri;
+  }
+
+  /// @notice Returns the base uri
+  /// @dev Overrides the standard _baseURI() functionality of the ERC721 standard to fit our needs
+  /// @return _baseURIextended The base uri
+  function _baseURI() internal view virtual override returns (string memory) {
+    return _baseURIextended;
+  }
+
+  /// @notice Creates the token uri based off of a given token id by concatenation (rings a bell?)
+  /// @dev Overrides the standard tokenURI() functionality of the ERC721 standard to fit our needs
+  /// @param _id The token id
+  /// @return => The token uri
+  function tokenURI(
+    uint256 _id
+  ) public view virtual override returns (string memory) {
+    return string.concat(_baseURI(), _id.toString(), '.json');
+  }
+
+  /// @notice Mints NFT's. Minting in this case means creating the NFT and assigning the minter as owner
+  /// @param _amount The amount of tokens that will be minted
+  function mint(uint256 _amount) public payable mintCompliance(_amount) {
+    ///@dev usage of tx.origin is fine here but do your own research (dyor) on why tx.origin can be dangerous
+    uint256 startId = totalSupply;
+    totalSupply += _amount;
+    userMintCount[msg.sender] += _amount;
+    for (uint256 i = 0; i < _amount; i++) {
+      _mint(msg.sender, startId + i);
+    }
+  }
+
+  /// @notice Modifier that adds minting requirements
+  modifier mintCompliance(uint _amount) {
+    require(msg.sender == tx.origin, 'No minting from contract allowed');
+    require(totalSupply + _amount <= maxSupply, 'Amount exceeds max supply');
+    require(_amount > 0, 'Amount cannot be zero');
+    require(userMintCount[msg.sender] + _amount <= maxPerUser, 'Not allowed to mint this many');
+    _;
+  }
+}
---
+++ smart-contracts/tests/Challenge_12.t.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.6.0 <0.9.0;
+
+import 'forge-std/Test.sol';
+
+import '@smart-contracts/Challenge_12.sol';
+
+contract Challenge12Test is Test {
+  using Strings for uint256;
+  Challenge12 c;
+  string baseURI = 'ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/';
+
+  function setUp() public {
+    c = new Challenge12('NFT Example', 'NFTEX', 100);
+    c.setBaseURI(baseURI);
+  }
+
+  /// @notice Checks that the tokenURI is properly constructed
+  /// @param _id The token id
+  /// TODO: Fix the test!
+  function testTokenUri(uint256 _id) public {
+    assertEq(c.tokenURI(_id), string.concat(baseURI, _id.toString(), '.json'));
+  }
+
+  /// @notice Non-owners should not be able to set base URI
+  /// @dev vm.startPrank() and the like are Foundry cheat codes
+  /// @dev More info amount cheat codes: https://book.getfoundry.sh/cheatcodes/
+  function testFailSetBaseURI() public {
+    vm.startPrank(address(1337));
+    c.setBaseURI('1337');
+    vm.stopPrank();
+  }
+
+  /// @notice Owners should be able to set the base URI
+  /// TODO: Fix the test!
+  function testSetBaseURI() public payable {
+    uint256 id = 69;
+    string memory newURI = '1337';
+    c.setBaseURI(newURI);
+    assertEq(c.tokenURI(id), string.concat(newURI, id.toString(), '.json'));
+  }
+
+  /// @notice Test minting 1 from Contract Account (CA)
+  /// @dev vm.expectRevert() expects the line below to revert with the message params
+  function testMintCA() public {
+    console.log('Minting from contract...');
+    vm.expectRevert('No minting from contract allowed');
+    c.mint(1);
+  }
+
+  /// @notice Test minting 1 from Externally Owned Account (EOA)
+  function testMintEOA() public {
+    console.log('Minting from EOA...');
+    uint256 initialSupply = c.totalSupply();
+    uint256 amount = 1;
+    vm.startPrank(msg.sender);
+    vm.expectRevert();
+    c.ownerOf(0);
+    assertEq(initialSupply, 0);
+    c.mint(amount);
+    assertEq(c.totalSupply(), amount);
+    assertEq(c.ownerOf(amount - 1), msg.sender);
+    vm.stopPrank();
+  }
+
+  /// @notice Fuzz test (within vm assumption constraints) minting max amount allowed
+  /// @param amount The amount to mint
+  /// @dev More about fuzz testing: https://book.getfoundry.sh/forge/fuzz-testing?highlight=fuzz%20testing#fuzz-testing
+  function testMintMax(uint256 amount) public {
+    uint256 maxSupply = c.maxSupply();
+    /// @dev Set the user max mint per wallet to the max allowed for this test
+    c.setUserMaxMint(2 ** 256 - 1);
+    vm.assume(amount > 0);
+    vm.assume(amount <= maxSupply);
+    vm.startPrank(msg.sender);
+    c.mint(amount);
+    assertEq(c.totalSupply(), amount);
+    vm.stopPrank();
+  }
+
+  /// @notice Fuzz test minting over max supply
+  /// @param amount The amount to mint
+  function testMintOverMax(uint256 amount) public {
+    /// @dev Set the user max mint to the max allowed for this test
+    c.setUserMaxMint(2 ** 256 - 1);
+    uint256 maxSupply = c.maxSupply();
+    vm.assume(amount > maxSupply);
+    vm.startPrank(msg.sender);
+    vm.expectRevert('Amount exceeds max supply');
+    c.mint(amount);
+    vm.stopPrank();
+  }
+
+  /// @notice Test minting 0 NFT's
+  function testMintZero() public {
+    vm.startPrank(msg.sender);
+    vm.expectRevert('Amount cannot be zero');
+    c.mint(0);
+    vm.stopPrank();
+  }
+
+  /// @notice Users should not be able to mint more than allowed per wallet
+  function testMintOverAllowed(uint amount, address user) public {
+    uint maxPerUser = c.maxPerUser();
+    uint maxSupply = c.maxSupply();
+    vm.assume(amount > maxPerUser);
+    vm.assume(amount <= maxSupply);
+    /// @dev User mints
+    vm.expectRevert('Not allowed to mint this many');
+    vm.prank(user, user);
+    c.mint(amount);
+  }
+}
