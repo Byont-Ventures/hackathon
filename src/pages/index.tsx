17073cb7490209a834246c81d5ef8c50862c3723
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-23 15:35:00 +0100

    feat: add list of links to challenge pages WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
+  const CHALLENGE_AMOUNT = 5
+  const links: string[] = Array.from(
+    { length: CHALLENGE_AMOUNT },
+    (_, idx) => `challenge-${idx + 1}`
+  )
+
   return (
-    <>
-      Welcome to the Workshop! By Byont Ventures B.V. ©{' '}
-      {new Date().getFullYear()}
-    </>
+    <div className="flex flex-col">
+      <div>Welcome to the Workshop!</div>
+      <div className="w-auto px-8 my-1">
+        <ol>
+          {links.map((link: string) => (
+            <Link key={link} href={`/${link}`}>
+              <li>{link}</li>
+            </Link>
+          ))}
+        </ol>
+      </div>
+      <div>By Byont Ventures B.V. © {new Date().getFullYear()}</div>
+    </div>
   )
 }
 
