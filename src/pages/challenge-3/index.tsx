7e5113d72741d429afbd9007be8de7dcbec012ed
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-23 17:21:14 +0100

    feat: add challenge-3 rough

---
+++ src/pages/challenge-3/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      Welcome to Challenge 3!
+      <ConnectButton></ConnectButton>
+    </>
+  )
+}
+
+export default Home
