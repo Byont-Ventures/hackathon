45c42aa4e9c19cfed99c3889f9145e49d7883573
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-23 18:14:46 +0100

    feat: add challenge 5 WIP

---
+++ src/pages/challenge-5/index.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from 'next'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useBalance } from 'wagmi'
+import { useAccount } from 'wagmi'
+import { useIsMounted } from 'src/hooks/useIsMounted'
+import { useContractRead } from 'wagmi'
+
+import { BAYCAbi } from 'src/abis/BAYCAbi'
+import { ReactNode } from 'react'
+
+const Home: NextPage = () => {
+  const account = useAccount()
+  const isMounted = useIsMounted()
+  const balance = useBalance({
+    addressOrName: account.address,
+  })
+  const { data, isError, isLoading } = useContractRead({
+    address: '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D',
+    abi: BAYCAbi,
+    functionName: 'name',
+  })
+
+  return (
+    <>
+      Welcome to Challenge 5!
+      <ConnectButton></ConnectButton>
+      <div>
+        {' '}
+        {balance && isMounted
+          ? `Account balance: ${balance.data?.value}`
+          : 'No data'}
+      </div>
+      {isMounted && isLoading ? (
+        'Loading...'
+      ) : (
+        <>{isMounted && <div>{data as ReactNode}</div>}</>
+      )}
+      <div> {isError && isMounted && 'Error'}</div>
+    </>
+  )
+}
+
+export default Home
