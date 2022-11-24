94fdea085bc8f16f96aee12d75bfcd7e2be14d31
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-24 16:33:20 +0100

    docs: add todo message

--- src/pages/challenge-2/index.tsx
+++ src/pages/challenge-2/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <>Welcome to Challenge 2!</>
+  return (
+    <>
+      Welcome to Challenge 2!
+      {/* TODO: Implement wallet connect button using RainbowKit */}
+    </>
+  )
 }
 
 export default Home
