097342d47ef141350e8d1302c953a601e866368c
sebastiaan <sebastiaancrisan@byont.nl>
2022-11-24 18:05:51 +0100

    docs: add TODOs

--- src/pages/challenge-3/index.tsx
+++ src/pages/challenge-3/index.tsx
@@ -2,6 +2,12 @@ import type { NextPage } from 'next'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 const Home: NextPage = () => {
+  /*
+   * TODO:
+   * - Implement the BAYC ABI
+   * - Display the name of the BAYC contract
+   * - Display the symbol of the BAYC contract
+   */
   return (
     <>
       Welcome to Challenge 3!
