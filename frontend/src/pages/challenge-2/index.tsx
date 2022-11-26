a2a11172f7f8f80d478f52378674fd0e6345ef15
Jasper Verbeet <jasperverbeet@gmail.com>
2022-11-26 17:48:52 +0100

    fix: updates challenge 2

--- frontend/src/pages/challenge-2/index.tsx
+++ frontend/src/pages/challenge-2/index.tsx
@@ -1,12 +1,29 @@
 import type { NextPage } from 'next';
+import { useBlockNumber } from 'wagmi';
 
-const Home: NextPage = () => {
+const Challenge2: NextPage = () => {
   /*
    * TODO:
-   * - Implement wallet connect button
-   * - Show account balance
+   * - Setup wagmi with the Alchemy provider
+   * - Display the latest blocknumber
    */
-  return <>Welcome to Challenge 2!</>;
+  const { data: blockNumber, isLoading, isError } = useBlockNumber();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError)
+    return (
+      <div>
+        Something went wrong while getting the latest blocknumber, did you setup
+        Wagmi correctly?
+      </div>
+    );
+
+  return (
+    <p>
+      The name is: <span className="text-green-400">#{blockNumber}</span>
+    </p>
+  );
 };
 
-export default Home;
+export default Challenge2;
