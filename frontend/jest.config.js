8d1f742197effdeb08cc6d42d3c991709ca9dde0
Jasper Verbeet <jasperverbeet@gmail.com>
2022-11-26 09:23:39 +0100

    refactor: splits configuration files for frontend and backend

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,29 +1,21 @@
-// jest.config.js
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextJest = require('next/jest')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextJest = require('next/jest');
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
-// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
+  testMatch: ['<rootDir>/**/*.test.tsx'],
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.env.js'],
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/src/components/$1',
-    '^@/libs/(.*)$': '<rootDir>/src/libs/$1',
-    '^@/tests/(.*)$': '<rootDir>/src/tests/$1',
-    '^@/images/(.*)$': '<rootDir>/src/images/$1',
+    ...pathsToModuleNameMapper(compilerOptions.paths),
     uuid: require.resolve('uuid'),
   },
-}
+};
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
